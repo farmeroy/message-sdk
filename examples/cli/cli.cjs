6e6f0beecf443b1715593d8fd62c32cba5e11e48
Raffaele Cataldo <kemanicataldo@gmail.com>
2026-08-28 19:37:11 -0700

    feat: examples cli for cjs and mjs

---
+++ examples/cli/cli.cjs
@@ -0,0 +1,14 @@
+const {createMessage} = require("@agent-message-sdk/core")
+
+function main() {
+  if (process.argv.length < 3) {
+    console.error("missing argument: message");
+    process.exitCode = 1;
+    return;
+  }
+  const m = process.argv[2]
+  const message = createMessage(m, "user")
+  console.log({message})
+}
+
+main()
