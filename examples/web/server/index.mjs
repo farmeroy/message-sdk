33770655d1c07958666ee7af822d3d38aaaec105
Raffaele Cataldo <kemanicataldo@gmail.com>
2026-09-05 11:14:15 -0700

    feat: add basic web server

---
+++ examples/web/server/index.mjs
@@ -0,0 +1,36 @@
+// @ts-check
+import Fastify from 'fastify'
+
+import { Client } from "@agent-message-sdk/node";
+
+const fastify = Fastify({
+  logger: true
+})
+
+fastify.get('/health', function (request, reply) {
+  reply.send("healthy")
+})
+
+fastify.post('/', async function (request, reply) {
+  const clientMessage = request.body.message;
+  const client = new Client("be concise");
+  try {
+    const clientResponse = await client.sendMessage(clientMessage);
+    const text = clientResponse.value.content[0].text;
+    reply
+      .code(200)
+      .send(text);
+  } catch (e) {
+    reply
+      .code(500)
+      .send({error: e})
+  }
+})
+
+fastify.listen({port: 8080}, function (err, address) {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  fastify.log.info(`server listening on ${address}`)
+})
